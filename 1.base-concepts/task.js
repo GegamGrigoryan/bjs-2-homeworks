a2de9f2b2cd25286f8ebb712e5d90df2b22119c6
Test <m-mik@ro.ru>
2022-12-01 17:11:55 +0300

    исправил ошибки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let d = (b ** 2) - 4 * a * c;
+  const discriminant = (b ** 2) - 4 * a * c;
   let arr = [];
 
-  if (d < 0) {
-    arr = [];
-  }
-
-  if (d === 0) {
+  if (discriminant === 0) {
     let root = -b / (2 * a);
     arr.push(root)
   }
 
-  if (d > 0) {
+  if (discriminant > 0) {
     let decisionOne = (-b + Math.sqrt(d)) / (2 * a);
     let decisionTwo = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(decisionOne)
@@ -21,6 +17,7 @@ function solveEquation(a, b, c) {
 
   return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let payment;
@@ -36,7 +33,7 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   payment =
     bodyCredit * (monthPer + monthPer / ((1 + monthPer) ** countMonths - 1))
   sumCount = payment * countMonths;
-  Math.floor(sumCount * 100) / 100;
+  (sumCount * 100) / 100;
 
-  return sumCount
+  return sumCount.toFixed()
 }
