b934c2a010fd7be7a7e3abbbfb09f5b23c6a3d85
Test <m-mik@ro.ru>
2022-12-13 02:21:25 +0300

    Массивы, выполнил задание.13.12

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    const comparisonResult = arr1.length === arr2.length &&
+        arr1.every((value, index) => value === arr2[index])
+
+    return comparisonResult
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users
+        .filter(user => user.gender === gender)
+        .map((user) => user.age)
+        .reduce((acc, item, index, array) => item / array.length + acc, 0)
+
 }
