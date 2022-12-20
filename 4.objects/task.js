2fd160638f17ca17469cbcb599817f29b20e24d6
Test <m-mik@ro.ru>
2022-12-20 10:30:02 +0300

    Объекты и Классы

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age, ...marks) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = marks;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.addMarks.hasOwnProperty('marks') !== undefined) {
+        this.marks.push(...marksToAdd)
+    } else {
+        console.log('Студент отчислен');
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || (this.marks.length < 1)) {
+        return 0
+    } else {
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i++) {
+            sum += this.marks[i];
+        }
+        return sum / this.marks.length
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
