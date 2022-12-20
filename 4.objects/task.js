fd94795da102a3adcb942bb4630423d5010b68f0
Test <m-mik@ro.ru>
2022-12-20 17:49:12 +0300

    Объекты исправил ошибки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,7 +2,7 @@ function Student(name, gender, age, ...marks) {
     this.name = name;
     this.gender = gender;
     this.age = age;
-    this.marks = marks;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -10,23 +10,20 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.addMarks.hasOwnProperty('marks') !== undefined) {
+    if (this.marks) {
         this.marks.push(...marksToAdd)
-    } else {
-        console.log('Студент отчислен');
     }
 }
 
 Student.prototype.getAverage = function () {
     if (this.marks === undefined || (this.marks.length < 1)) {
         return 0
-    } else {
-        let sum = 0;
-        for (let i = 0; i < this.marks.length; i++) {
-            sum += this.marks[i];
-        }
-        return sum / this.marks.length
     }
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+    return sum / this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
